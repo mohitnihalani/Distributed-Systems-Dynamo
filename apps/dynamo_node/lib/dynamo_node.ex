0260c380e38c07833a87da9c2f83685bea168585
Mohit Nihalani <nihalanim9@gmail.com>
2020-11-23 09:29:09 -0500

    Something changes

--- apps/dynamo_node/lib/dynamo_node.ex
+++ apps/dynamo_node/lib/dynamo_node.ex
@@ -6,7 +6,15 @@ defmodule DynamoNode do
   require Fuzzers
   require Logger
   import Ring
+  """
+  Dynamo Node
+  Each node has it's view of the state, which basically means each node has its own ring
+  Node can be initiated using the seed node or without the seed node
+  Seed node is used so that node can connect to the ring when joined initially
+  If seed node is provided in the confguration, it will try to connect to the seed node using gossip protocol
+  and join the states.
 
+  """
   defstruct(
     state: nil,
     version: nil,
@@ -30,12 +38,8 @@ defmodule DynamoNode do
     node = %DynamoNode{state: hash_ring, version: nil, pid: whoami(), seed_node: seed_node}
   end
 
-  defp synch_with_seed_node(node) do
-    if node.seed_node == nil do
-      node
-    else
-
-    end
+  defp sync_state(ring1, ring2) do
+    Ring.sync_rings(ring1, ring2)
   end
 
   @spec get_gossip_timeout(%DynamoNode{}) :: non_neg_integer()
@@ -47,7 +51,7 @@ defmodule DynamoNode do
     %{node | heartbeat_timer: timer}
   end
 
-  defp set_gossip_timeout(node) do
+  defp reset_gossip_timeout(node) do
     if node.heartbeat_timer, do: Emulation.cancel_timer(node.heartbeat_timer)
     gossip_timeout = get_gossip_timeout(node)
     save_heartbeat_timer(node, Emulation.timer(node.heartbeat_timeout))
@@ -55,5 +59,34 @@ defmodule DynamoNode do
 
   def join_ring(state, otherstate) do
     Ring.sync_rings(state, otherstate)
+  end
+
+  def run_gossip_protocol(node) do
+    :rand.seed(:exrop, {101, 102, 103})
+    random_node = Enum.random(node.state.nodes)
+    send(random_node, {:share_state, node.state})
+    node = reset_gossip_timeout(node)
+
+    receive do
+      {_sender, {:share_state_reply, %Ring{} = joined_state}} ->
+        node = %{node | state: sync_state(node.state, joined_state)}
+        run_node(node)
+      :timer ->
+        run_node(node)
+    end
+  end
+
+  @spec lauch_node(%DynamoNode{}) :: no_return()
+  def lauch_node(node) do
+    if node.seed_node != nil do
+
+    end
+  end
+
+  """
+  This is where most recursion happend maintaing state
+  """
+  defp run_node(node) do
+
   end
 end
